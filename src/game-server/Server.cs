e548705d13c25a415189948397987e283844417b
Nick Larsen <fodylarsen@gmail.com>
2015-11-04 10:30:03 -0500

    server: clean up game play start code

--- src/game-server/Server.cs
+++ src/game-server/Server.cs
@@ -65,25 +65,16 @@ namespace GameServer
                 bots.Add(client);
                 if (bots.Count == 2)
                 {
-                    //PlayTicTacToe(bots[0], bots[1]);
-                    PlayNineMensMorris(bots[0], bots[1]);
+                    //PlayGame(new TicTacToeGame(bots[0], bots[1]));
+                    PlayGame(new NineMensMorrisGame(bots[0], bots[1]));
                 }
             }
         }
 
-        private void PlayTicTacToe(ClientConnection player1, ClientConnection player2)
+        private void PlayGame(Game game)
         {
-            var game = new TicTacToeGame(player1, player2);
             game.OnCompleted += GameCompletedHandler;
-            Log("Starting TicTacToe Game: " + game.GetDescription());
-            game.Start();
-        }
-
-        private void PlayNineMensMorris(ClientConnection player1, ClientConnection player2)
-        {
-            var game = new NineMensMorrisGame(player1, player2);
-            game.OnCompleted += GameCompletedHandler;
-            Log("Starting TicTacToe Game: " + game.GetDescription());
+            Log("Starting Game: " + game.GetDescription());
             game.Start();
         }
 
