5d9cdf1ac9424e1f57f9011659950cadf8ebca0e
Nick Larsen <fodylarsen@gmail.com>
2015-11-19 11:07:13 -0500

    protected constructor for abstract class

--- src/game-server/Game.cs
+++ src/game-server/Game.cs
@@ -20,7 +20,7 @@ namespace GameServer
         private string currentStatus;
         private readonly int millisPerMove;
 
-        public Game(ClientConnection player1, ClientConnection player2, GameRules<TState> rules, int millisPerMove)
+        protected Game(ClientConnection player1, ClientConnection player2, GameRules<TState> rules, int millisPerMove)
         {
             this.player1 = player1;
             this.player2 = player2;
